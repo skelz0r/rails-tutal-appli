2b136487f67e179adee661e79e8d42d999f611e7
Loïc Delmaire <loic.delmaire@gmail.com>
2013-04-10 15:42:02 +0200

    Deep inside : Modification du model ActiveRecord
    - Essayez d'ajouter un article sans titre maintenant !

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ActiveRecord::Base
   attr_accessible :body, :title
+
+  # Validation de la présence du titre
+  # Si le titre est manquant, le ressource ne peut pas
+  # être sauvegardée
+  # Plus d'infos : http://api.rubyonrails.org/classes/ActiveModel/Validations/HelperMethods.html#method-i-validates_presence_of
+  validates_presence_of :title
 end
