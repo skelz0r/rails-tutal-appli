6913b92e7cf478c74001e0a832caa54dfeec2c90
Loïc Delmaire <loic.delmaire@gmail.com>
2013-04-10 15:22:24 +0200

    Commit initial
    - Commande effectué : rails new rails-tutal-appli

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTutalAppli::Application.config.secret_token = '2e9ba2d4a4678a7179547f28e70a8aa182710fafad2e336aae430e33f26935d66a098e21d13e6666ce907560e8474d3eaa0d727dcb8301f71e013796dd1f6d64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsTutalAppli::Application.config.session_store :cookie_store, key: '_rails-tutal-appli_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsTutalAppli::Application.config.session_store :active_record_store
